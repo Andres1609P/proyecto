4f6d59517d03e54f797c6fbc1ab46055446e512b
Andres1609P <aperezquesada@outlook.es>
2024-12-09 21:26:38 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var MiImagen=document.querySelector("Cambiar");
+
+function PrimeraImagen{
+MiImagen.src="\imagenes\gato-comiendo.jpg";
+
+}
+function SegundaImagen{
+MiImagen.src="alimentacion-natural-para-gatos.jpg";
+
+}
+
